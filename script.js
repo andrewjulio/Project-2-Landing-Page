1770be3f8cde723d60385e66d479513fdf651baa
Andrew JT <andrewjulio@hotmail.com>
2019-10-21 19:51:48 +0200

    Added the Sections via JS and conditional to include Download button in Navbar

--- script.js
+++ script.js
@@ -3,22 +3,43 @@ function createNav() {
 
     const navItems = ['About', 'Features', 'Pricing', 'Testimonials', 'Download'];
 
-    const ul = document.createElement('ul')
-
-    ul.className = "navbar";
-
-    const navigation = document.getElementById('navigation');
+    const ul = document.querySelector('ul')
 
     for (let navItem of navItems) {
         let li = document.createElement("li");
-        ul.appendChild(li);
-        li.innerHTML += `<a href="#content-${navItem.toLowerCase()}">${navItem}</a>`;
-        li.id = navItem;
-        li.className = "navbar-item box";
-    }
+        if (navItem == 'Download') {
+            li.innerHTML += `<a href="#content-${navItem.toLowerCase()}"><button>${navItem}</button></a>`;
+            li.id = navItem;
+            li.className = "navbar-item box";
+            ul.appendChild(li);
+        }
+        else {
+            li.innerHTML += `<a href="#content-${navItem.toLowerCase()}">${navItem}</a>`;
+            li.id = navItem;
+            li.className = "navbar-item box";
+            ul.appendChild(li);
+        }
 
-    navigation.appendChild(ul);
+    }
 
 }
 
 createNav();
+
+
+function createSec() {
+
+    const sectionNames = ['About', 'Features', 'Pricing', 'Download', 'Testimonials'];
+
+    const content = document.getElementById('content');
+
+    for (let sectionName of sectionNames) {
+        section = document.createElement("section");
+        section.className = "content box";
+        section.id = `content-${sectionName.toLowerCase()}`;
+        content.appendChild(section);
+    }
+
+}
+
+createSec();
